7a2ec590a8fd8a5a3373abe293161d6a9077278b
Philip Sanders <mrcmyk12@gmail.com>
2021-04-12 20:04:37 -0700

    Added Header and Footer Component

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,69 +1,65 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 
 
-class CampsiteInfo extends Component{
 
-    renderCampsite(campsite) {
-        return(
-            <div className='col-md-5 m-1'>
-                <Card >
-                    <CardImg top src={campsite.image} alt={this.props.campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
-            </div>
-        )
-    }
 
-    renderComments(comments) {
-
-        if (comments){
-            return(
-                <div className="div col-5-md m-1" key={this.props.campsite.id}>
-                    <h4>Comments</h4>
-                        {comments.map( comment => 
-                            <div className='p-1' key={comment.id}>
-                              {comment.text}<br></br>
-                              --{comment.author}, {new Intl.DateTimeFormat('en-US', 
-                              { year: 'numeric', month: 'short', day: '2-digit'}).format
-                              (new Date(Date.parse(comment.date)))}
-                            </div>
-                            
-                         
-                    )}
-                </div>
-            );
-        };
-        return <div />
-
-    }
-
-  
-    render() {
-
-        
-        if (this.props.campsite) {
-
-           return(
-            <div className="container">
-                <div className='row' key='this.props.campsite.id'>
-                {this.renderCampsite(this.props.campsite)}
-                {this.renderComments(this.props.campsite.comments)}
-                </div>
-            </div>
-            );
-        }
-        
-           return(
-            <div />
-           );
-        
-
-    }
+function RenderCampsite({campsite}) {
+    return(
+        <div className='col-md-5 m-1'>
+            <Card >
+                <CardImg top src={campsite.image} alt={campsite.name} />
+                <CardBody>
+                    <CardTitle>{campsite.name}</CardTitle>
+                    <CardText>{campsite.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    )
 }
 
+function RenderComments({comments}) {
+
+    if (comments){
+        return(
+            <div className="div col-5-md m-1" key={comments.id}>
+                <h4>Comments</h4>
+                    {comments.map( comment => 
+                        <div className='p-1' key={comment.id}>
+                        {comment.text}<br></br>
+                        --{comment.author}, {new Intl.DateTimeFormat('en-US', 
+                        { year: 'numeric', month: 'short', day: '2-digit'}).format
+                        (new Date(Date.parse(comment.date)))}
+                        </div>
+                        
+                    
+                )}
+            </div>
+        );
+    };
+    return <div />
+
+}
+
+  
+function CampsiteInfo(props) {
+
+        
+if (props.campsite) {
+
+    return(
+        <div className="container">
+            <div className='row' key='this.props.campsite.id'>
+                <RenderCampsite campsite={props.campsite} />
+                <RenderComments comments={props.campsite.comments} />
+            </div>
+        </div>
+        );
+    }
+return <div />;
+}
+
+
 export default CampsiteInfo;
+
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Directory from './DirectoryComponent';
 
+import Directory from './DirectoryComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import { CAMPSITES } from '../shared/campsites';
 
@@ -24,13 +25,11 @@ onCampsiteSelect(campsiteId) {
   render() {
       return (
           <div>
-              <Navbar dark color="primary">
-              <div className="container">
-                  <NavbarBrand href="/">NuCamp</NavbarBrand>
-              </div>
-              </Navbar>
+              <Header />
               <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
-              <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} /> 
+              <CampsiteInfo campsite={this.state.campsites.filter
+                (campsite => campsite.id === this.state.selectedCampsite)[0]} /> 
+              <Footer />
           </div>
       );
   }
